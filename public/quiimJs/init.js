1b0fc6d5b3077a8eb3944acf6c052c9f8b6df218
Projetoquiim <projetoquim@gmail.com>
2026-05-25 15:43:16 -0300

    Alteração do init.js

--- public/quiimJs/init.js
+++ public/quiimJs/init.js
@@ -1,25 +1,31 @@
-import {listar,logar, listaArquivos} from "./firebase/ini.js";
+import { listar, logar, listaArquivos } from "./firebase/ini.js";
 
 const usuario = document.querySelector("#email");
 const senha = document.querySelector("#password");
 const btnEntrar = document.querySelector("#entrar");
 const btnGoogle = document.querySelector("#google");
 
-btnEntrar.addEventListener("click", (e)=>{
+btnEntrar.addEventListener("click", (e) => {
     logar(usuario.value, senha.value);
-    
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 1500
+    });
 });
 
 
-const listagem = listar().then((lista)=>{
+const listagem = listar().then((lista) => {
     console.log("Dados => ", lista);
-}).catch((error)=>{
+}).catch((error) => {
     console.error("Erro ao listar dados:", error);
 });
 
-const arquivos = listaArquivos().then((lista)=>{
+const arquivos = listaArquivos().then((lista) => {
     console.log("Arquivos => ", lista);
-}).catch((error)=>{
+}).catch((error) => {
     console.error("Erro ao listar arquivos:", error);
 });
 
